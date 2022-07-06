542f3f4f0ac7c40b97221ecbc7ec99c691ebb53b
GOPIKA-S-S <gopikass1024@gmail.com>
2022-07-06 23:47:42 +0530

    viewfood completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SearchRestaurantComponent } from './search-restaurant/search-restaurant.component';
+import { ViewfoodComponent } from './viewfood/viewfood.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SearchRestaurantComponent
+    SearchRestaurantComponent,
+    ViewfoodComponent
   ],
   imports: [
     BrowserModule,
